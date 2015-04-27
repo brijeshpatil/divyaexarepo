5d9587d2973a9200988e60cadd9dbc399a8a0439
brijeshpatil <brijesh.patil@tops-int.com>
2015-04-27 10:01:58 +0530

    Uploaded Project Files

---
+++ WebForm1.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" DataKeyNames="userid" OnRowCancelingEdit="GridView1_RowCancelingEdit" OnRowCommand="GridView1_RowCommand" OnRowDeleting="GridView1_RowDeleting" OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating" ShowHeaderWhenEmpty="true">
+                <Columns>
+                    <asp:TemplateField HeaderText="UserID">
+                        <ItemTemplate>
+                            <asp:Label ID="Label1" runat="server" Text='<%#Bind("userid") %>'></asp:Label>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:Label ID="Label2" runat="server" Text='<%#Bind("userid") %>'></asp:Label>
+                        </EditItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:Label ID="Label3" runat="server" Text='<%#Bind("fname") %>'></asp:Label>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtfname" runat="server" Text='<%#Bind("fname") %>'></asp:TextBox>
+                        </EditItemTemplate>
+                        <HeaderTemplate>
+                            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                        </HeaderTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:Label ID="Label4" runat="server" Text='<%#Bind("lname") %>'></asp:Label>
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtlname" runat="server" Text='<%#Bind("lname") %>'></asp:TextBox>
+                        </EditItemTemplate>
+                        <HeaderTemplate>
+                            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>&nbsp;
+                            <asp:Button ID="Button1" runat="server" Text="Add" CommandName="ADD" />
+                        </HeaderTemplate>
+                    </asp:TemplateField>
+                    <asp:CommandField ShowEditButton="true" />
+                    <asp:CommandField ShowDeleteButton="true" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=PC339\SQL2008;Initial Catalog=asdasd;Integrated Security=True");
+        SqlDataAdapter da;
+        DataTable dt;
+        SqlCommand cmd;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                FillGRid();
+            }
+        }
+
+        private void FillGRid()
+        {
+            da = new SqlDataAdapter("select * from userinfo",con);
+            dt = new DataTable();
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            FillGRid();
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int UserID = Convert.ToInt16(GridView1.DataKeys[e.RowIndex].Value);
+            cmd = new SqlCommand("delete from userinfo where userid=@UID", con);
+            cmd.Parameters.AddWithValue("@UID", UserID);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            FillGRid();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            FillGRid();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            TextBox txtfname = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtfname");
+            TextBox txtlname = (TextBox)GridView1.Rows[e.RowIndex].FindControl("txtlname");
+            int UserID = Convert.ToInt16(GridView1.DataKeys[e.RowIndex].Value);
+            cmd = new SqlCommand("update userinfo set fname=@Fname,lname=@Lname where userid=@UID", con);
+            cmd.Parameters.AddWithValue("@Fname", txtfname.Text);
+            cmd.Parameters.AddWithValue("@Lname", txtlname.Text);
+            cmd.Parameters.AddWithValue("@UID", UserID);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            GridView1.EditIndex = -1;
+            FillGRid();
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "ADD")
+            {
+                TextBox txtfname = (TextBox)GridView1.HeaderRow.FindControl("TextBox1");
+                TextBox txtlname = (TextBox)GridView1.HeaderRow.FindControl("TextBox2");
+                cmd = new SqlCommand("insert into userinfo values(@Fname,@Lname)",con);
+                cmd.Parameters.AddWithValue("@Fname", txtfname.Text);
+                cmd.Parameters.AddWithValue("@Lname", txtlname.Text);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                FillGRid();
+            }
+        }
+    }
+}
